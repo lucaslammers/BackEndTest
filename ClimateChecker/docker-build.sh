2303a1466be4741f00fee03e5167fb2220552aa1
Lucas <68347235+lucaslammers@users.noreply.github.com>
2024-01-09 21:39:37 +0100

    Update docker-build.sh

--- ClimateChecker/docker-build.sh
+++ ClimateChecker/docker-build.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+DOCKER_REGISTRY="localhost:5000"
+
 IMAGE_TAG="ontdekstation-server"
 VERSION="${1-latest}"
 
 # script takes an optional tag argument, otherwise uses "latest"
 docker build \
     -f /var/lib/jenkins/workspace/BackEndTest/ClimateChecker/Dockerfile \
-    -t "$IMAGE_TAG:$VERSION" .
+    -t "$DOCKER_REGISTRY/$IMAGE_TAG:$VERSION" .
 
-echo "$IMAGE_TAG:$VERSION"
+docker push "$DOCKER_REGISTRY/$IMAGE_TAG:$VERSION"
+
+echo "Image pushed to $DOCKER_REGISTRY/$IMAGE_TAG:$VERSION"
