ff48481fca6d018a6699a55810869b9b89e46e45
Lucas <68347235+lucaslammers@users.noreply.github.com>
2024-01-09 18:48:29 +0100

    Update docker-build.sh

--- ClimateChecker/docker-build.sh
+++ ClimateChecker/docker-build.sh
@@ -7,7 +7,7 @@ VERSION="${1-latest}"
 
 # script takes an optional tag argument, otherwise uses "latest"
 docker build \
-    -f Dockerfile \
+    -f /var/lib/jenkins/workspace/BackEndTest/ClimateChecker/Dockerfile \
     -t "$IMAGE_TAG:$VERSION" .
 
 echo "$IMAGE_TAG:$VERSION"
